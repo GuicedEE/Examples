9b73555ed69c82e1ee0ade6abfd7396ea31e0981
GedMarc <ged_marc@hotmail.com>
2020-11-10 00:57:15 +0200

    V1.0.20.2 - 2020/11/010 * WS-RT - 3.0.0 * Apache FOP Module Split from OpenPDF * PrimeFaces Showcase Updates

--- Faces/PrimeFaces-Showcase/src/main/java/org/primefaces/showcase/view/misc/PrimeIconsView.java
+++ Faces/PrimeFaces-Showcase/src/main/java/org/primefaces/showcase/view/misc/PrimeIconsView.java
@@ -15,9 +15,9 @@
  */
 package org.primefaces.showcase.view.misc;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.primefaces.shaded.json.JSONArray;
+import org.primefaces.shaded.json.JSONException;
+import org.primefaces.shaded.json.JSONObject;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
